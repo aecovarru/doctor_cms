4574cc07af4e9175dec1a7916c335c93116452a3
aecovarru <aecovarru@gmail.com>
2014-08-20 13:55:35 -0700

    Test

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,8 @@ Bundler.require(*Rails.groups)
 
 module DoctorCms
   class Application < Rails::Application
-    config.time_zone = 'Pacific Time (US & Canada)'
-    config.active_record.default_timezone = 'Pacific Time (US & Canada)'
+    # config.time_zone = 'Pacific Time (US & Canada)'
+    # config.active_record.default_timezone = 'Pacific Time (US & Canada)'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
